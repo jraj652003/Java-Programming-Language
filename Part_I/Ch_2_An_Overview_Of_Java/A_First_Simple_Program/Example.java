b7b11dd62b0f64495135031dcccd3880f0d69f95
Jayaraj N <jraj66650@gmail.com>
2024-11-14 09:48:35 +0530

    A First Simple Program

---
+++ Part_I/Ch_2_An_Overview_Of_Java/A_First_Simple_Program/Example.java
@@ -0,0 +1,13 @@
+package Part_I.Ch_2_An_Overview_Of_Java.A_First_Simple_Program;
+
+/*
+ * This is a simple Java program.
+ * Call this file "Example.java".
+*/
+
+class Example {
+    // Your program begins with a call to main()
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+}
